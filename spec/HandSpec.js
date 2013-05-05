09de8ef4477d81224f302eb8e29ae493fbb50015
Dan Sabin <dan.sabin@gmail.com>
2013-05-05 13:27:11 -0400

    here's more working hands

--- spec/HandSpec.js
+++ spec/HandSpec.js
@@ -2,9 +2,26 @@ describe("Test a hand works.", function() {
 
 	var hand = new Hand();
 
+    beforeEach(function() {
+        hand = new Hand();
+    });
+
+    afterEach(function() {
+        hand = new Hand();
+    });
+    
 	it("The hand can have cards added to it", function () {
-		var card = new Card(7, SUIT.HEART);
-        hand.addCard(card);
+		var seven = new Card(7, SUIT.HEART);
+        hand.addCard(seven);
 		expect(hand.count()).toBe(1);
+	});
+    
+    it("The hand canshow you the cards it has.", function () {
+    	var seven = new Card(7, SUIT.HEART);
+        var ace = new Card(1, SUIT.HEART);
+        hand.addCard(seven);
+        hand.addCard(ace);
+		expect(hand.count()).toBe(2);
+        expect(hand.toString()).toBe("7 H A H");
 	});
 });
--- src/Hand.js
+++ src/Hand.js
@@ -10,10 +10,24 @@ Hand.prototype.addCard = function(card) {
 	this.cards.push(card);
 };
 
+Hand.prototype.toString = function() {
+    if (this.isEmpty()) {
+		return "Empty Hand";
+	} else {
+		var revealFirst = this.cards[0].toString() + " ";
+
+		this.cards.every(function(card){
+            revealFirst += card.toString();
+        });
+
+		return revealFirst.trim();
+	}    
+};
+
 Hand.prototype.clear = function() {
     this.cards = [];
 };
 
 Hand.prototype.isEmpty = function() {
-    return this.cards.length() === 0;
+    return this.cards.length === 0;
 };
