24189b382d1186f1b51c75abd9e2726c3d031f02
Dan Sabin <dan.sabin@gmail.com>
2013-05-05 18:05:22 -0400

    new completeness check

--- spec/DeckSpec.js
+++ spec/DeckSpec.js
@@ -1,6 +1,27 @@
 describe("Test a deck works.", function() {
 
     var deck = new Deck();
+    
+    var testDeckForCompleteness = function(deck) {
+        var threeCount = 0;
+    	var jackCount = 0;
+		var aceCount = 0;
+
+		for (var i = 0; i < DECK_SIZE; i++)
+		{
+			var card = deck.dealCard();
+			if (card.toString().equals("3")) {
+				threeCount++;
+			}
+			if (card.toString().equals("J")) {
+				jackCount++; 
+			}
+			if (card.toString().equals("A")) {
+				aceCount++;
+			}
+		}
+		return threeCount === NUMBER_OF_SUITS && jackCount === NUMBER_OF_SUITS && aceCount === NUMBER_OF_SUITS; 
+    };
 
     beforeEach(function() {
         deck = new Deck();
@@ -23,5 +44,8 @@ describe("Test a deck works.", function() {
         expect(deck.cardsRemaining()).toBe(49);
 	});
     
+    it("A new deck has the right number of cards in it.", function () {
+		expect(testDeckForCompleteness(deck)).toBe(true);
+	});
     
 });
